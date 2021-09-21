1714efbbd78f0dc00ef22fbbd785ba11ed9adc81
aamirfayaz <dropmeataamir@gmail.com>
2021-09-21 10:31:56 +0530

    done with printable type class implementation

--- src/main/scala/cats/typeclasses/TypeClassesPrintable.scala
+++ src/main/scala/cats/typeclasses/TypeClassesPrintable.scala
@@ -1,5 +1,7 @@
 package cats.typeclasses
 
+final case class Cat(name: String, age: Int, color: String)
+
 trait Printable[A] {
   def format(a: A):String
 }
@@ -11,9 +13,44 @@ object PrintableInstances {
   implicit val stringInstance = new Printable[String] {
     override def format(a: String): String = a
   }
+
+  implicit val catInstance = new Printable[Cat] {
+    override def format(c: Cat): String = {
+      s"Name is ${c.name}, AGE ${c.age}, COLOR ${c.color}"
+    }
+  }
 }
 
 object Printable {
   def format[A](value: A)(implicit print: Printable[A]):String = print.format(value)
-  def print[A](value: A)(implicit print: Printable[A]):Unit = println(format(value))
+  def printt[A](value: A)(implicit print: Printable[A]):Unit = println(format(value))
+}
+
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](a: A) {
+    def format(implicit p: Printable[A]): String = {
+      p.format(a)
+    }
+    def print(implicit p: Printable[A]): Unit = {
+      println(format)
+    }
+  }
+}
+
+object PrintIntefaceObjectCheck extends App {
+
+  import Printable._
+  import PrintableInstances._
+
+  println(s"Format: ${format(10001)}")
+  printt("James")
+}
+
+
+object PrintIntefaceSyntaxCheck extends App {
+  import PrintableSyntax._
+  import PrintableInstances._
+
+  Cat("Timmy", 4, "Blue").print
 }

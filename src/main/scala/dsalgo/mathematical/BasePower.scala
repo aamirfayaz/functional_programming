6d51fb0252e58366942935a20d4c2471795267d1
aamirfayaz <dropmeataamir@gmail.com>
2021-10-05 13:46:45 +0530

    started math for CS

--- src/main/scala/dsalgo/mathematical/BasePower.scala
+++ src/main/scala/dsalgo/mathematical/BasePower.scala
@@ -2,7 +2,7 @@ package dsalgo.mathematical
 
 object BasePower extends App {
 
-   def calculate(base: Int, power: Int, acc: Int):Int = {
+   def calculate(base: Int, power: Int, acc: Int = 1):Int = {
      if(power == 0) acc else {
        calculate(base, power - 1, acc * base)
      }
@@ -10,4 +10,13 @@ object BasePower extends App {
   println(calculate(2, 3, 1))
   println(calculate(4, 3, 1))
   println(calculate(3, 3, 1))
+}
+
+object SumBasePowerTillN extends App {
+  /**
+   * We can count to 1023 by just using two hands
+   */
+  println {
+    (1 to 9).map(x => BasePower.calculate(2, x)).sum + 1
+  }
 }

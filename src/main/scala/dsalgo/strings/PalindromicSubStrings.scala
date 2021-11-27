46951b81c9205be26174b516418f1b1ea4ff690b
aamirfayaz <dropmeataamir@gmail.com>
2021-11-28 00:12:36 +0530

    palindromic sub strings of a string

---
+++ src/main/scala/dsalgo/strings/PalindromicSubStrings.scala
@@ -0,0 +1,44 @@
+package dsalgo.strings
+
+import scala.annotation.tailrec
+
+object PalindromicSubStrings extends App {
+
+
+    /*
+       abc = a, b, c, ab, bc, abc
+       i = 0, j = 1 ,2, 3
+       i = 1, j = 2, 3
+       i = 2, j = 3
+     */
+    def printSubStrings(s: String): Unit = {
+        for(i <- 0 until s.length) {
+            for(j <- i + 1 to s.length) {
+                println(s.substring(i, j))
+            }
+        }
+    }
+    def isPalindrome(s: String): Boolean = {
+        @tailrec
+        def rec(s: String, start: Int = 0, end: Int): Boolean = {
+            if(start < end) {
+                if(s(start) != s(end)) false else rec(s, start + 1, end - 1)
+            } else true
+        }
+        rec(s, end = s.length - 1)
+    }
+
+    def printPalindromicSubStrings(s: String):Unit = {
+        for(i <- 0 until s.length) {
+            for(j <- i + 1 to s.length) {
+                if(isPalindrome(s.substring(i, j))) println(s.substring(i, j))
+            }
+        }
+    }
+
+    //println(isPalindrome("arora"))
+    //println(isPalindrome("madam"))
+    //println(isPalindrome("hello"))
+    //printSubStrings("abc")
+    printPalindromicSubStrings("abac")
+}
--- src/main/scala/scalaconcepts/generics/test2.sc
+++ src/main/scala/scalaconcepts/generics/test2.sc
@@ -7,3 +7,19 @@ trait Combine2[-T] {
   def item[B <: T]: B
   def combineWith[B <: T](t: B):B
 }
+
+case class CombineInt(x: Int) extends Combine1[Int] {
+
+  override def item:Int = x
+
+  override def combineWith[B >: Int](t: B) = {
+    item
+  }
+}
+
+
+ val c: Combine1[Int] = CombineInt(10)
+ val d = c.item
+
+
+

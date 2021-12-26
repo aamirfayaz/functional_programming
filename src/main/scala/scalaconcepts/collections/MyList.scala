3393224fa421ef5b5c4acd18a5e214beb99ddabd
aamirfayaz <dropmeataamir@gmail.com>
2021-12-26 23:21:27 +0530

    functions empty to unit

--- src/main/scala/scalaconcepts/collections/MyList.scala
+++ src/main/scala/scalaconcepts/collections/MyList.scala
@@ -8,18 +8,18 @@ trait MyList[+A] {
   def prepend[B >: A](item: B): MyList[B]
   //0(n)
   def append[B >: A](item: B): MyList[B]
-  def map[B](f: A => B):MyList[B]
-  def filter(f: A => Boolean):MyList[A]
+//  def map[B](f: A => B):MyList[B]
+ // def filter(f: A => Boolean):MyList[A]
 }
 
 case class MyCons[+A](head: A, tail: MyList[A]) extends MyList[A] {
    def isEmpty: Boolean = false
    def prepend[B >: A](item: B): MyList[B] = MyCons(item, this)
    def append[B >: A](item: B): MyList[B] = MyCons(head, tail.append(item))
-   def map[B](f: A => B):MyList[B] = MyCons(f(head), tail.map(f))
-   def filter(f: A => Boolean):MyList[A] = {
-     if(f(head)) MyCons(head, tail.filter(f)) else tail.filter(f)
-   }
+ //  def map[B](f: A => B):MyList[B] = MyCons(f(head), tail.map(f))
+ //  def filter(f: A => Boolean):MyList[A] = {
+  //   if(f(head)) MyCons(head, tail.filter(f)) else tail.filter(f)
+  // }
 }
 
 case object EmptyList extends MyList[Nothing] {
---
+++ src/main/scala/scalaconcepts/functions/EmptyToUnitFunctions.scala
@@ -0,0 +1,22 @@
+package scalaconcepts.functions
+
+object EmptyToUnitFunctions extends App {
+  private val initCode = new scala.collection.mutable.ListBuffer[() => Unit]
+
+  def delayExecution(body: => Unit) = {
+    initCode += (() => body)
+  }
+  val res:Unit = Thread.sleep(6000)
+  delayExecution(res)
+
+  for (proc <- initCode) proc()
+
+  //println(sys.props("scala.time"))
+}
+
+object EmptyT1 extends App {
+
+  val justDoSomething: () => Int = () => 3
+  println(justDoSomething) // function itself
+  println(justDoSomething()) // call
+}

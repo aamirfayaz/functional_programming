c207bcd4807d392fa5801c573a57261e2b774a37
aamirfayaz <dropmeataamir@gmail.com>
2022-01-04 17:31:00 +0530

    interview here

---
+++ src/main/scala/pastinterviews/FutureBlocking.scala
@@ -0,0 +1,9 @@
+/**
+ *
+ * Three threads call a method
+ * unless first is not finished other should wait for it to finish execution
+ *
+ * means, ist call1 should finish then second then third
+ *
+ * inside put Thread.sleep
+ */
